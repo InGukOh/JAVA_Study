b2997d7c826bb4e182ff2b06b9d92cbc08fc3c5c
Trident12kilo <dhdlsrnr1231@naver.com>
2022-04-28 17:32:10 +0900

    코딩테스트_프로그래머스 핸드폰 번호 가리기

---
+++ src/pack_JavaStudy/Block_Number.java
@@ -0,0 +1,29 @@
+package pack_JavaStudy;
+
+public class Block_Number {
+
+	public static void main(String[] args) {
+		String phone_number = "01033334444";
+		String answer = "";
+		
+		String[] PNArr = phone_number.split("");
+        int count = phone_number.length();
+		int replaceCount = 0;        
+        for(String change : PNArr) {
+			if(replaceCount < count-4) {
+				change = "*";
+			}
+			answer +=change;
+			replaceCount++;
+		}
+        System.out.println(answer + "1");
+		
+		answer = "";
+		for (int i = 0; i < phone_number.length() - 4; i++) {
+	         answer += "*";
+	    }
+		System.out.println(answer + phone_number.substring(phone_number.length() - 4) + "2");
+		
+		System.out.println(phone_number.replaceAll(".(?=.{4})", "*") + "3");
+	}
+}
