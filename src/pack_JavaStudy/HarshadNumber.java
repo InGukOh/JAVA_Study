53efcc51f084b48c7caa5edea481706d25047c40
Trident12kilo <dhdlsrnr1231@naver.com>
2022-04-28 17:49:19 +0900

    하샤드의 수

---
+++ src/pack_JavaStudy/HarshadNumber.java
@@ -0,0 +1,22 @@
+package pack_JavaStudy;
+
+public class HarshadNumber {
+
+	public static void main(String[] args) {
+		int x = 13;
+		boolean answer = true;
+		
+		String[] Arr  = Integer.toString(x).split("");
+		int sum = 0;
+		for (int i = 0; i < Arr.length; i++) {
+			sum += Integer.parseInt(Arr[i]);
+		}
+		if(x%sum !=0) {
+			answer = false;
+		}
+		
+		System.out.println(sum);
+		System.out.println(answer);
+	}
+//  https://programmers.co.kr/learn/courses/30/lessons/12947/solution_groups?language=java&type=all
+}
