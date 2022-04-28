aaa6540322e4b57be9be98bdb1b7e8c1645c4228
Trident12kilo <dhdlsrnr1231@naver.com>
2022-04-28 18:05:31 +0900

    프로그래머스 / 평균 구하기

---
+++ src/pack_JavaStudy/GetMean.java
@@ -0,0 +1,15 @@
+package pack_JavaStudy;
+
+public class GetMean {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4};
+		for (int i = 1; i < arr.length; i++) {
+			arr[0] += arr[i];
+			System.out.println(arr[0]);
+		}
+		double res = arr[0]/Double.valueOf(arr.length);
+		System.out.printf("%f",res);
+	}
+
+}
