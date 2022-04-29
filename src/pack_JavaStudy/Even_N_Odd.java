c55228819ca0246190fec36d1fee1962760ff189
Trident12kilo <dhdlsrnr1231@naver.com>
2022-04-29 20:22:06 +0900

    프로그래머스 홀수와 짝수

---
+++ src/pack_JavaStudy/Even_N_Odd.java
@@ -0,0 +1,18 @@
+package pack_JavaStudy;
+
+import java.util.Scanner;
+
+public class Even_N_Odd {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int set = sc.nextInt();
+		String answer = "Odd";
+		if(set%2 == 0 ) {
+			answer = "Even";
+		}
+		System.out.println(answer);
+
+	}
+}
+//https://programmers.co.kr/learn/courses/30/lessons/12937/solution_groups?language=java
