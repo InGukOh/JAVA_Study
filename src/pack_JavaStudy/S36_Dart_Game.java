ef447bf62f56bc65c5bd3addc0d695afc82fc783
Trident12kilo <dhdlsrnr1231@naver.com>
2022-06-13 17:50:51 +0900

    22/06/13 S36 Dart Game

--- src/pack_JavaStudy/S36_Dart_Game.java
+++ src/pack_JavaStudy/S36_Dart_Game.java
@@ -11,16 +11,16 @@ public class S36_Dart_Game {
        
         int idx = -1;
         for (int i=0; i<carr.length; i++) {
-            if (carr[i] >= '0' && carr[i] <= '9') {
+            if (carr[i] >= '0' && carr[i] <= '9') { //숫자인 경우
                 idx++; 
-                round[idx] += Integer.parseInt(String.valueOf(carr[i]));
+                round[idx] += Integer.parseInt(String.valueOf(carr[i])); //문자열 -> 정수열
       
-                if (i+1!=carr.length-1 && carr[i+1] == '0'&& carr[i] == '1') {
+                if (i+1!=carr.length-1 && carr[i+1] == '0' && carr[i] == '1') {//문자열 내 다음이 0이거나 1인경우
                     round[idx] -= Integer.parseInt(String.valueOf(carr[i]));
                     round[idx] = 10;
                     i++;                    
                 }                
-            } else if (carr[i] == 'D') {
+            } else if (carr[i] == 'D') { //문자열 구분
                 round[idx] *= round[idx];
             } else if(carr[i] == 'T') { 
                 round[idx] *= round[idx] * round[idx];
