c5b9e998ccc3aa026df5d7c8a116063004e27f06
Trident12kilo <dhdlsrnr1231@naver.com>
2022-06-15 18:48:33 +0900

    22/06/15 S29 Animal Sick

--- src/pack_JavaStudy/SQL/Animal_SQL.sql
+++ src/pack_JavaStudy/SQL/Animal_SQL.sql
@@ -13,7 +13,8 @@ insert into Animal_ins values('A349996','Cat','2018-01-22 14:32:00','Normal','Su
 insert into Animal_ins values('A350276','Cat','2017-08-13 13:50:00','Normal','Jewel','Spayed Female');
 insert into Animal_ins values('A396810','Dog','2016-08-22 16:13:00','Injured','Raven','Spayed Female');
 insert into Animal_ins values('A410668','Cat','2015-11-19 13:41:00','Normal','Raven','Spayed Female');
-select Animal_ID,Name from Animal_INS where INTAKE_CONDITION !='Aged' order by Animal_ID asc; /*22/06/14 S30*/
+select Animal_ID,Name from Animal_INS where INTAKE_CONDITION ='Sick' order by Animal_ID asc;/*22/06/15 S29*/
+select Animal_ID,Name from Animal_INS where INTAKE_CONDITION !='Aged' order by Animal_ID asc; /*22/06/14 S31*/
 select Animal_ID,Name from Animal_INS order by Animal_ID asc;/*22/06/14 S30*/
 select Animal_ID,Name,DateTime from Animal_INS order by Name asc , dateTime desc; /*22/06/13 S_34*/
 select * from Animals_INS order by DATETIMES asc;
