ec919c0fcf251891edf1c8734a15240bc788f96e
Trident12kilo <dhdlsrnr1231@naver.com>
2022-06-17 22:11:45 +0900

    S23_Divisers Cound & Sum

---
+++ src/pack_JavaStudy/S23_Divisers_Count_Sum.java
@@ -0,0 +1,22 @@
+package pack_JavaStudy;
+
+public class S23_Divisers_Count_Sum {
+
+	public static void main(String[] args) {
+		int left = 13;
+		int right = 17;
+		int answer = 0;
+		for (int i=left;i<=right;i++) {
+		     
+            if (i % Math.sqrt(i) == 0) {
+                answer -= i;
+            }
+            
+            else {
+                answer += i;
+            }
+        }
+		System.out.println(answer);
+	}
+
+}
