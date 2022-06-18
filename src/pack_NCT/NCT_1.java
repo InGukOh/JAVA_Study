f3195738ad54abcdb0be20cc6bde165d1a396742
Trident12kilo <dhdlsrnr1231@naver.com>
2022-06-19 01:13:00 +0900

    네이버 부스트캠프 풀이 1

---
+++ src/pack_NCT/NCT_1.java
@@ -0,0 +1,13 @@
+package pack_NCT;
+
+public class NCT_1 {
+
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 1; i <= 100; i+=7) {
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
