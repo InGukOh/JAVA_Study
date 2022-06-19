45a65f52e75ccad879fd8ce0fd3ce728a5ac4842
Trident12kilo <dhdlsrnr1231@naver.com>
2022-06-20 05:52:02 +0900

    22/06/20 S20 Animal Name is Null!

--- bin/pack_JavaStudy/SQL/Animal_SQL.sql
+++ bin/pack_JavaStudy/SQL/Animal_SQL.sql
@@ -12,7 +12,9 @@ SEX_UPON_INTAKE		VARCHAR(90) not null
 insert into Animal_ins values('A349996','Cat','2018-01-22 14:32:00','Normal','Sugar','Neutered Male');
 insert into Animal_ins values('A350276','Cat','2017-08-13 13:50:00','Normal','Jewel','Spayed Female');
 insert into Animal_ins values('A396810','Dog','2016-08-22 16:13:00','Injured','Raven','Spayed Female');
-insert into Animal_ins values('A410668','Cat','2015-11-19 13:41:00','Normal','Raven','Spayed Female');
+insert into Animal_ins values('A410668','Cat','2015-11-19 13:41:00','Normal',null,'Spayed Female');
+select * from Animal_INS;
+select Animal_ID from Animal_INS where Name is null order by Animal_ID asc;/*22/06/20 S26*/
 select Name,DateTime from Animal_INS order by Animal_ID desc;/*22/06/15 S26*/
 select Animal_ID from Animal_INS where Name != null order by Animal_ID asc;/*22/06/15 S27*/
 select Animal_ID,Name from Animal_INS where INTAKE_CONDITION ='Sick' order by Animal_ID asc;/*22/06/15 S29*/
