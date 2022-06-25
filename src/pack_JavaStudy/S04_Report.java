6c7515c49e8b4d1587defde320edcd3cfe7d0d3b
Trident12kilo <dhdlsrnr1231@naver.com>
2022-06-26 04:36:01 +0900

    22/06/26 S4 Report //마지막문제 시작

---
+++ src/pack_JavaStudy/S04_Report.java
@@ -0,0 +1,20 @@
+package pack_JavaStudy;
+
+import java.util.Arrays;
+
+public class S04_Report {
+
+	public static void main(String[] args) {
+		String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+		String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+		int k = 2;
+		int[] getRep= new int[id_list.length];
+		
+		for (int i = 0; i < report.length; i++) {
+			String[] check = report[i].split(" ");
+			
+		}
+		int[] answer = {};
+	}
+
+}
