42cd0dfa4a57c5ed54c3ca63d0cd0bd012ed8d6d
Trident12kilo <dhdlsrnr1231@naver.com>
2022-06-26 06:36:47 +0900

    22/06/26 S04 Report // 해설추가 . 그래도 이해하는데 시간이 필요했다.

--- src/pack_JavaStudy/S04_Report.java
+++ src/pack_JavaStudy/S04_Report.java
@@ -16,20 +16,21 @@ public class S04_Report {
 		int k = 2;
 		Map<String,List<String>> map= new HashMap<>();
         Map<String,Integer> mail_map = new HashMap<>();
-        // 모든 유저에 대해 신고자리스트를 생성
-        // Key: 유저, Value: 유저를 신고한 사람들
+        // 모든 유저에 대해 신고자리스트를 생성 {muzi=[], neo=[], frodo=[], apeach=[]}
+        // Key: 유저, Value: 유저를 신고한 사람들 {muzi=0, neo=0, frodo=0, apeach=0}
         for(String user: id_list){
             List<String> list = new LinkedList<>();
             map.put(user,list);         
             mail_map.put(user,0);    
         }
         
-        System.out.println(map);
-        System.out.println(mail_map);
+        //System.out.println(map);
+       // System.out.println(mail_map);
         
         // 신고자: attacker 신고당한자 : defender
-        // 신고당한 건수를 처리함       
-        for(String temp: report){
+        // 신고당한 건수를 처리함
+        
+        for(String temp : report){
             String[] arr=temp.split(" ");
             String attacker=arr[0];
             String defender=arr[1];           
@@ -42,8 +43,8 @@ public class S04_Report {
             map.put(defender,list);        
         }         
         
-        System.out.println(map);
-        System.out.println(mail_map);
+        //System.out.println(map);
+        //System.out.println(mail_map);
         
         // K번 신고당한 사람을 찾고, 신고한 사람들에게 Count증가
         for(String data: map.keySet()){
@@ -56,8 +57,8 @@ public class S04_Report {
             }
         }
         
-        System.out.println(map);
-        System.out.println(mail_map);
+        //System.out.println(map);
+        //System.out.println(mail_map);
         
         int i=0;
         int[] answer = new int[id_list.length];
